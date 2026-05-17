23a832b7328c820d6eb388c6dcdc2875fc7ba42e
charles ono <investorshi20@gmail.com>
2026-05-16 15:31:17 -1200

    `Updated Smartsupp Live Chat script keys in app.blade.php and user.blade.php`

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -87,7 +87,7 @@
 <!-- Smartsupp Live Chat script -->
 <script type="text/javascript">
 var _smartsupp = _smartsupp || {};
-_smartsupp.key = 'b29911edd839e6acb1a916b3bd40f276486047f3';
+_smartsupp.key = '8b9c7bca81883a893eb40d0489e8e4ea53dcfb6f';
 window.smartsupp||(function(d) {
   var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
   s=d.getElementsByTagName('script')[0];c=d.createElement('script');
@@ -95,7 +95,8 @@ window.smartsupp||(function(d) {
   c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
 })(document);
 </script>
-<noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
+<noscript>Powered by <a href="https://chartmasterscircle.com/" target="_blank">ChartmastersCircle</a></noscript>
+
 
 
 
