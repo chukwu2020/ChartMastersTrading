3eae81628836340b4e148055cc38328ef736955e
charles ono <investorshi20@gmail.com>
2026-08-15 11:53:56 -1200

    updated smartsupp

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -86,10 +86,9 @@
 
 
 <!-- Smartsupp Live Chat script -->
-<!-- Smartsupp Live Chat script -->
 <script type="text/javascript">
 var _smartsupp = _smartsupp || {};
-_smartsupp.key = '857c054cdf880d973007384a87332b837287110c';
+_smartsupp.key = 'cd1b92e3d06cb7d8c3aa612a36bf5d08be3942b1';
 window.smartsupp||(function(d) {
   var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
   s=d.getElementsByTagName('script')[0];c=d.createElement('script');
@@ -97,6 +96,7 @@ window.smartsupp||(function(d) {
   c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
 })(document);
 </script>
+
 
 
 
